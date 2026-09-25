bf8ea3066a20d47ba451ba38f5ee7000e69f301c
Jackson4Rocks <leonsonyk@gmail.com>
2026-09-25 16:46:57 +0530

    rebrand: update Calypso ISO profile identity

--- iso/profiledef.sh
+++ iso/profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="amethyst-os"
+iso_name="calypso-linux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Amethyst OS"
-iso_application="Amethyst OS (AURORA) Live"
+iso_publisher="Calypso Linux"
+iso_application="Calypso Linux — Aurora Dotfiles Live"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
